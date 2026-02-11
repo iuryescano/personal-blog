25d5efc853dc8ed5cdeb86cfb70df97ed33ef967
iury.cordeiro <iurycordeiro15@gmail.com>
2026-02-11 17:40:36 -0300

    ✨feature: Header created with new ActiveLink button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,9 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Header } from "@/components/header";
 
 export default function Home() {
   return (
-    <div className="min-h-full min-w-full bg-slate-500">
-      <h2 className="text-2xl font-bold">Hello, World!</h2>
+    <div>
+      <Header />
     </div>
   );
 }
