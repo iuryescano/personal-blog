33d11a6e37c21da242099b952f61f7c929aa0d12
iury.cordeiro <iurycordeiro15@gmail.com>
2026-02-11 18:33:17 -0300

    ✨feature: Create Footer in the app and add layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
-import { Header } from "@/components/header";
+import { Geist, Geist_Mono} from "next/font/google";
+
+const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geistsans",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geistmono",
+});
 
 export default function Home() {
   return (
-    <div>
-      <Header />
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      
     </div>
   );
 }
