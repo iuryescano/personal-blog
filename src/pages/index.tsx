234b0e2d0fc24e4cb0f5263dde540bfa8c04c2f7
iury.cordeiro <iurycordeiro15@gmail.com>
2026-02-17 20:21:17 -0300

    ✨feature: hero-section created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,11 @@
-import { Geist, Geist_Mono} from "next/font/google";
-
-const geistSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-geistsans",
-});
-
-const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geistmono",
-});
+import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      
-    </div>
+    <>
+      <article className="flex flex-col">
+        <HeroSection />
+      </article>
+    </>
   );
 }
