8d2f7102b661a9daa66b83a47493b2ead9fc95ff
iury.cordeiro <iurycordeiro15@gmail.com>
2026-04-08 16:49:55 -0300

    ✨feature: customer-story-section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
+import { SupportSection } from "@/components/support-section";
+import { CustomerStorySection } from "@/components/cutomer-story-section";
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <article className="flex flex-col">
         <HeroSection />
         <FeatureSection />
+        <SupportSection />
+        <CustomerStorySection />
       </article>
     </>
   );
