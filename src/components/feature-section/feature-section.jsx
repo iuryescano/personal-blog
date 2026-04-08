83916d87a82823afb428e43732bc65f4f3000398
iury.cordeiro <iurycordeiro15@gmail.com>
2026-04-08 19:48:03 -0300

    Customer story section ✨feature: call-to-action finished

--- src/components/feature-section/feature-section.jsx
+++ src/components/feature-section/feature-section.jsx
@@ -8,7 +8,7 @@ export const FeatureSection = ({ }) => {
     <section className="container bg-gray-700 grid gap-6 md:grid-cols-2 pb-8 pt-8 md:py-10">
       <div className="flex flex-col gap-4 rounded-lg p-6 bg-gray-500 md:p-12">
         <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">Simples</span>
-        <h2 className="text-gray-100  text-heading-lg">
+        <h2 className="text-gray-100  text-heading-lg font-sans">
           Crie um catalogo de produtos online em poucos minutos
         </h2>
       </div>
@@ -16,7 +16,7 @@ export const FeatureSection = ({ }) => {
       <div className="flex flex-col gap-4 rounded-lg p-6 bg-gray-500 md:p-12">
         <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">Prático
         </span>
-        <h2 className="text-gray-100  text-heading-lg">
+        <h2 className="text-gray-100  text-heading-lg font-sans">
           Venda para o seu público através de uma plataforma única
         </h2>
       </div>
@@ -27,7 +27,7 @@ export const FeatureSection = ({ }) => {
             <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">
               Personalizável
             </span>
-            <h2 className="text-gray-100  text-heading-lg">
+            <h2 className="text-gray-100  text-heading-lg font-sans">
               Tenha uma loja personalizada com a cara da sua marca
             </h2>
 
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,16 +1,27 @@
 import { Footer } from "../footer/footer"
 import { Header } from "../header/header"
 import { Inter } from 'next/font/google'
+import { PT_Sans_Caption } from "next/font/google"
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  variable: '--font-inter',
+})
+
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ["latin"],
+  weight: ["700"],
+  variable: '--font-pt-sans',
+})
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}>
+    <div className={`${inter.className} ${ptSansCaption.className} relative flex min-h-screen flex-col bg-gray-700 font-inter`}>
       <Header />
       <main className="flex-1 flex flex-col mt-10 mb-12">
         {children}
